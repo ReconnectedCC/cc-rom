cd9c131fc63819a60c7495362d7d9a5a487c5a7a
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2024-08-01 11:02:32 +0200

    improve grammar

--- data/computercraft/lua/rom/programs/turtle/place.lua
+++ data/computercraft/lua/rom/programs/turtle/place.lua
@@ -3,8 +3,8 @@ local selected = turtle.getSelectedSlot()
 
 if slot then
 	selected = tonumber(slot)
-	assert(selected, "slot must be a number")
-	assert(selected >= 1 and selected <= 16, "slot must be between 1 and 16")
+	assert(selected, "Slot must be a number")
+	assert(selected >= 1 and selected <= 16, "Slot must be between 1 and 16")
 end
 
 turtle.select(selected)
