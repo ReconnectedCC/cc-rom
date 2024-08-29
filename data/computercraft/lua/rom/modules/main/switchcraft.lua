5f4a66ba17816f303da1e958d05dbd603847db4c
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2024-08-29 19:18:46 +0200

    copy over switchcraft.lua

---
+++ data/computercraft/lua/rom/modules/main/switchcraft.lua
@@ -0,0 +1,66 @@
+local GITHUB_API_URL = "https://api.github.com"
+
+function githubLimits(key)
+  key = key or "guest"
+  local headers = {}
+
+  local url = GITHUB_API_URL .. "/rate_limit"
+  if key ~= "guest" then
+    headers.Authorization =  'token ' .. key
+  end
+
+  local h, err = http.get(url, headers)
+  if not h or err then
+    error("Error contacting GitHub API: " .. err)
+  end
+
+  local data = textutils.unserializeJSON(h.readAll())
+  h.close()
+  return data
+end
+
+function tps()
+  local h = http.get("https://api.ct.knijn.one/tps")
+  if not h then
+      return 0, "Error contacting TPS API"
+  end
+  local data = h.readAll()
+  h.close()
+  return tonumber(data), nil
+end
+
+function isYemmelOn()
+  return false, "the brotherhood is real"
+end
+isYemOn = isYemmelOn
+
+function isDrunk(n)
+  local h, err = http.get("https://lemmmy.pw/bac?u=" .. textutils.urlEncode(n))
+  if err then error("Not ok") end
+  
+  local data = textutils.unserialiseJSON(h.readAll())
+  h.close()
+  return data.drunk
+end
+
+function isYemmelDrunk()
+  return isDrunk("Yemmel")
+end
+isYemDrunk = isYemmelDrunk
+
+function isLemmmyDrunk()
+  return isDrunk("Lemmmy")
+end
+isLemDrunk = isLemmmyDrunk
+
+return {
+  githubLimits = githubLimits,
+  tps = tps,
+  isYemmelOn = isYemmelOn,
+  isYemOn = isYemmelOn,
+  isDrunk = isDrunk,
+  isYemmelDrunk = isYemmelDrunk,
+  isYemDrunk = isYemmelDrunk,
+  isLemmmyDrunk = isLemmmyDrunk,
+  isLemDrunk = isLemmmyDrunk
+}
