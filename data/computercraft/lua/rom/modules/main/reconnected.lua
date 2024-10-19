140ef4f5709f593b3b20f2d817596a4a09eecc16
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2024-10-19 10:28:53 +0200

    change grammar

--- data/computercraft/lua/rom/modules/main/reconnected.lua
+++ data/computercraft/lua/rom/modules/main/reconnected.lua
@@ -1,7 +1,12 @@
 local function tps()
     local h = http.get("https://api.reconnected.cc/tps")
     if not h then
-        return 0, "Error contacting TPS API"
+        return 0, "Error contacting the API"
+    end
+    local data = h.readAll()
+    h.close()
+    return tonumber(data), nil
+end
     end
     local data = h.readAll()
     h.close()
--- data/computercraft/lua/rom/modules/main/switchcraft.lua
+++ data/computercraft/lua/rom/modules/main/switchcraft.lua
@@ -22,7 +22,7 @@ end
 function tps()
   local h = http.get("https://api.reconnected.cc/tps")
   if not h then
-      return 0, "Error contacting TPS API"
+      return 0, "Error contacting the API"
   end
   local data = h.readAll()
   h.close()
