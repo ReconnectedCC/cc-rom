9cc0f17c2ca2b9f0ff0e8d64ba6929cb5bc35161
Riley Thrailkill <rt@hellscaped.dev>
2025-04-26 17:36:35 -0500

    Remove irrelavent directories.

--- data/computercraft/lua/rom/mcdata/computercraft/lua/rom/autorun/poster.completion.lua
+++
@@ -1,2 +0,0 @@
-local completion = require "cc.shell.completion"
-shell.setCompletionFunction("rom/programs/poster.lua", completion.build(completion.file))
--- data/computercraft/lua/rom/mcdata/computercraft/lua/rom/autorun/print3d.completion.lua
+++
@@ -1,2 +0,0 @@
-local completion = require "cc.shell.completion"
-shell.setCompletionFunction("rom/programs/print3d.lua", completion.build(completion.file))
